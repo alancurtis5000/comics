93bf0f37d94d29f4f721aaaa0659052b7d5a0e64
Alan <alancurtis5000+accounts@gmail.com>
2024-02-04 21:08:25 -0700

    backend push

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,15 +1,15 @@
 export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "comicsd54ef02b": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "HostedUIDomain": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "OAuthMetadata": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  }
-}
+  auth: {
+    comicsd54ef02b: {
+      AppClientID: "string";
+      AppClientIDWeb: "string";
+      HostedUIDomain: "string";
+      IdentityPoolId: "string";
+      IdentityPoolName: "string";
+      OAuthMetadata: "string";
+      UserPoolArn: "string";
+      UserPoolId: "string";
+      UserPoolName: "string";
+    };
+  };
+};
